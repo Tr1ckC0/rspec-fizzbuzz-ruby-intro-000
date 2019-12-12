b38bf50e30783666965f0097eb3ce3c7eee4fab4
Patrick Colina <trick.co@yahoo.com>
2019-12-12 03:19:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,10 @@
-def fizzbuzz(number)
-
+def fizzbuzz(int)
+  if int % 3 == 0
+    put "Fizz"
+  elsif int % 5 == 0
+    put "Buzz"
+  elsif int % 3 == 0 && int % 5 == 0
+    put "Fizzbuzz"
+  end
 end
+
